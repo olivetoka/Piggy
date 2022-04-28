758c5dd89b6f6b097f032e4303fa904845b36f4b
Kaylee Oliveto <43828195+olivetoka@users.noreply.github.com>
2022-04-28 18:54:12 +0000

    change to defult

--- student.py
+++ student.py
@@ -19,7 +19,7 @@ class Piggy(PiggyParent):
         '''
         self.LEFT_DEFAULT = 80
         self.RIGHT_DEFAULT = 80
-        self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
+        self.MIDPOINT = 1675  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
         
